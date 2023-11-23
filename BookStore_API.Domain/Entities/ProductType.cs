097b2fb66eadb8223031b60e2c4d382c6c7d6dc4
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-11-23 17:11:35 +0400

    fixed productType access modifier to public

--- BookStore_API.Domain/Entities/ProductType.cs
+++ BookStore_API.Domain/Entities/ProductType.cs
@@ -2,7 +2,7 @@
 
 namespace BookStore_API.Domain.Entities
 {
-    internal class ProductType : BaseEntity
+    public class ProductType : BaseEntity
     {
         public string Name { get; set; }
     }
