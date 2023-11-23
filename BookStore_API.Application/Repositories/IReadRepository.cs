398436570b81a256c85e0e15760a88aed9dc046c
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-11-23 15:31:47 +0400

    added IRepository, IReadRepository and IWriteRepository

---
+++ BookStore_API.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore_API.Application.Repostories;
+using System.Linq.Expressions;
+
+namespace BookStore_API.Application.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> method);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
+    }
+}
---
+++ BookStore_API.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore_API.Application.Repostories;
+
+namespace BookStore_API.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> UpdateAsync(T model);
+        Task<bool> RemoveAsync(T model);
+        Task<bool> RemoveAsync(string id);
+
+
+
+    }
+}
