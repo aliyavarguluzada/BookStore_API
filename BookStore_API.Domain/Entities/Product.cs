2affaea0c69c75f05327e1871b1d4e7877bf5104
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-11-23 17:10:24 +0400

    Added ProductType to differentiate Products

--- BookStore_API.Domain/Entities/Product.cs
+++ BookStore_API.Domain/Entities/Product.cs
@@ -7,6 +7,8 @@ namespace BookStore_API.Domain.Entities
         public string Name { get; set; }
         public long Price { get; set; }
         public int Stock { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductType ProductType { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
 }
---
+++ BookStore_API.Domain/Entities/ProductType.cs
@@ -0,0 +1,9 @@
+﻿using BookStore_API.Domain.Entities.Common;
+
+namespace BookStore_API.Domain.Entities
+{
+    internal class ProductType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
