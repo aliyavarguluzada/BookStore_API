0d0ef1a8dfe8d801e90483fce31f00087edbc3c8
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-12-01 18:44:44 +0400

    Adding fileUpload as service still not done

---
+++ BookStore_API.Application/Services/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BookStore_API.Application.Services
+{
+    public interface IFileService
+    {
+        Task UploadAsync(string path, IFormFileCollection files);
+        Task<string>FileRenameAsync(string fileName);
+        Task<bool> CopyFileAsync(string path, IFormFile file);
+    }
+}
---
+++ BookStore_API.Infrastructure/Services/FileService.cs
@@ -0,0 +1,55 @@
+﻿using BookStore_API.Application.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace BookStore_API.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<bool> CopyFileAsync(string path, IFormFile file)
+        {
+            try
+            {
+                await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+
+                await fileStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //TODO: logging!!!!
+                throw ex;
+            }
+
+        }
+
+        public Task<string> FileRenameAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UploadAsync(string path, IFormFileCollection files)
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            foreach (IFormFile file in files)
+            {
+                string fileNewName = await FileRenameAsync(file.FileName)
+
+                 await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
+            }
+
+        }
+    }
+}
