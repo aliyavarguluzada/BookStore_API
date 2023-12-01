a0daf98bdfbb4aefadc5b0efec1aeb160cf89fa2
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-12-01 19:08:49 +0400

    Added ServiceRegistration to Infrastructure layer for dependency injection

---
+++ BookStore_API.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using BookStore_API.Application.Services;
+using BookStore_API.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookStore_API.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void InfrastructureService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IFileService, FileService>();
+        }
+    }
+}
