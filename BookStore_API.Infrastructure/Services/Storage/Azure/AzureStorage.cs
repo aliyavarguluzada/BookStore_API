90a3160eafee6419e0e1584f80d873074076f5fd
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-12-15 15:18:45 +0400

    Azure blob does not work local works

--- BookStore_API.Infrastructure/Services/Storage/Azure/AzureStorage.cs
+++ BookStore_API.Infrastructure/Services/Storage/Azure/AzureStorage.cs
@@ -6,49 +6,49 @@ using Microsoft.Extensions.Configuration;
 
 namespace BookStore_API.Infrastructure.Services.Storage.Azure
 {
-    public class AzureStorage : IAzureStorage
+    public class AzureStorage : Storage, IAzureStorage
     {
-        private readonly BlobServiceClient _blobServiceCLient;
+        private readonly BlobServiceClient _blobServiceClient;
         BlobContainerClient _blobContainerClient;
 
         public AzureStorage(IConfiguration configuration)
         {
-            _blobServiceCLient = new(configuration["Storage:Azure"]);
+            _blobServiceClient = new(configuration["Storage:Azure"]);
         }
         public async Task DeleteAsync(string containerName, string fileName)
         {
-            _blobContainerClient = _blobServiceCLient.GetBlobContainerClient(containerName);
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
             BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
             await blobClient.DeleteAsync();
         }
 
         public List<string> GetFiles(string containerName)
         {
-            _blobContainerClient = _blobServiceCLient.GetBlobContainerClient(containerName);
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
             return _blobContainerClient.GetBlobs().Select(b => b.Name).ToList();
         }
 
         public bool HasFile(string containerName, string fileName)
         {
 
-            _blobContainerClient = _blobServiceCLient.GetBlobContainerClient(containerName);
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
             return _blobContainerClient.GetBlobs().Any(b => b.Name == fileName);
         }
 
         public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection files)
         {
-            _blobContainerClient = _blobServiceCLient.GetBlobContainerClient(containerName);
-
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
             await _blobContainerClient.CreateIfNotExistsAsync();
             await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
 
             List<(string fileName, string pathOrContainerName)> datas = new();
-
             foreach (IFormFile file in files)
             {
-                BlobClient blobClient = _blobContainerClient.GetBlobClient(file.Name);
+                string fileNewName = await FileRenameAsync(containerName, file.Name, HasFile);
+
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(fileNewName);
                 await blobClient.UploadAsync(file.OpenReadStream());
-                datas.Add((file.Name, containerName));
+                datas.Add((fileNewName, $"{containerName}/{fileNewName}"));
             }
             return datas;
         }
